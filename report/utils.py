3195bd41b90e862b9f5588d3eeed36afc1805ee0
Norra T <norramon.t@gmail.com>
2025-05-16 11:50:26 +0100

    included paths for project_root and model_path

--- report/utils.py
+++ report/utils.py
@@ -4,12 +4,17 @@ from pathlib import Path
 # Using the Path object, create a `project_root` variable
 # set to the absolute path for the root of this project directory
 #### YOUR CODE HERE
+ROOT_DIR = Path(__file__).resolve().parent.parent  
+project_root = ROOT_DIR
+# print(project_root)
  
 # Using the `project_root` variable
 # create a `model_path` variable
 # that points to the file `model.pkl`
 # inside the assets directory
 #### YOUR CODE HERE
+model_path = project_root/ 'assets/model.pkl'
+# print(model_path)
 
 def load_model():
 
